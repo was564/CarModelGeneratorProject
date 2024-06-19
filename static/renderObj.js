0af02e4309a19cafe9f0f85ccc863d29b17c5012
was564 <was56475@gmail.com>
2024-06-20 07:37:01 +0900

    renderObj import file change

--- static/renderObj.js
+++ static/renderObj.js
@@ -1,2 +1,21 @@
-import * as Three from 'three'
+import * as Three from './node_modules/three/three.module.js';
 
+const scene = new Three.Scene();
+const camera = new Three.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+const renderer = new Three.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+const geometry = new Three.BoxGeometry(1, 1, 1);
+const material = new Three.MeshBasicMaterial({ color: 0x00ff00 });
+const cube = new Three.Mesh(geometry, material);
+scene.add(cube);
+
+camera.position.z = 5;
+
+function animate() {
+    requestAnimationFrame(animate);
+    renderer.render(scene, camera);
+}
+animate();
